7a80e269f6892feb17d49e6c499b266474d11021
shion <shion@LAPTOP-I3HGA0DE>
2025-08-24 18:47:00 +0900

    a

---
+++ FileName2.cpp
@@ -0,0 +1,15 @@
+//変数に関するサンプルプログラム
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	a = 10;
+	b = 15;
+	c = 20;
+	cout << a << "足す" << b << "は" << c << endl;
+	return 0;
+}
